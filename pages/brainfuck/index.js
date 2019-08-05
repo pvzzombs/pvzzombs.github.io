68a715aeb3a9df75072f5dcb91d629973bf6dc52
Miles MJ Jamon <31880601+pvzzombs@users.noreply.github.com>
2019-08-04 23:20:49 -0700

    Add files via upload

--- pages/brainfuck/index.js
+++ pages/brainfuck/index.js
@@ -1,113 +1,94 @@
-function bf(str, input){
+function bf(array){
+  array = array.split("");
+  var memory = [0];
+  var pointer = 0;
+  var result = [];
+  var blocks = [];
+  var max = 4000000;
+  var maxCounter = 1;
 
-var array = str.replace(/(\w|\t|\s)/g,"").split("");
-
-var memory = new Uint8Array(20);
-
-var pointer = 0;
-
-var result = [];
-
-var open = [];
-
-var max = 0;
-
-var blocks = [];
-for(var i = 0; i < array.length; i++){
-  ++max;
-  
-  if(max == 1024*1024){
-    throw "Error: Maximum iteration exceeded!";
+  var counter = 0;
+  while(counter < array.length){
+    if(maxCounter === max){
+      break;
+    }
+  	if(array[counter] === ">"){
+  		pointer += 1;
+  		if(memory.length == pointer){
+  			memory.push(0);
+  		}
+  		blocks.push({cmd : ">", mem : memory[pointer], pnt : pointer});
+  		maxCounter++;
+  	}else if(array[counter] === "<"){
+  		pointer -= 1;
+  		if(pointer < 0){
+  			pointer = memory.length - 1;
+  		}
+  		blocks.push({cmd : "<", mem : memory[pointer], pnt : pointer});
+      maxCounter++;
+  	}else if(array[counter] === "+"){
+  		memory[pointer] += 1;
+  		if(memory[pointer] > 255){
+  			memory[pointer] = 0;
+  		}
+  		blocks.push({cmd : "+", mem : memory[pointer], pnt : pointer});
+      maxCounter++;
+  	}else if(array[counter] === "-"){
+  		memory[pointer] -= 1;
+  		if(memory[pointer] < 0){
+  			memory[pointer] = 255;
+  		}
+  		blocks.push({cmd : "-", mem : memory[pointer], pnt : pointer});
+      maxCounter++;
+  	}else if(array[counter] === "."){
+    	result.push(String.fromCharCode(memory[pointer]));
+    	blocks.push({cmd : ".", mem : memory[pointer], pnt : pointer, code : String.fromCharCode(memory[pointer])});
+      maxCounter++;
+  	}else if(array[counter] === ","){
+  		var pending = prompt("Input Command Detected - Enter a character : ", "");
+  		if(pending === ""){
+  			memory[pointer] = 0;
+  		}else{
+  			pending = pending || "A";
+    		memory[pointer] = parseInt(pending.charCodeAt(0));
+    	}
+    	blocks.push({cmd : ",", mem : memory[pointer], pnt : pointer});
+      maxCounter++;
+  	}else if(array[counter] === "["){
+  		if(memory[pointer] === 0){
+  			var open = 0;
+  			counter += 1;
+  			while(counter < array.length){
+  				if(array[counter] === "]" && open === 0){
+  					break;
+  				}else if(array[counter] === "["){
+  					open += 1;
+  				}else if(array[counter] === "]"){
+  					open -= 1;
+  				}
+  				counter += 1;
+  			}
+        maxCounter++;
+  		}
+  	}else if(array[counter] === "]"){
+  		if(memory[pointer] > 0){
+  			var close = 0;
+  			counter -= 1;
+  			while(counter >= 0){
+  				if(array[counter] === "[" && close === 0){
+  					break;
+  				}else if(array[counter] === "]"){
+  					close += 1;
+  				}else if(array[counter] === "["){
+  					close -= 1;
+  				}
+  				counter -= 1;
+  			}
+        maxCounter++;
+  		}
+  	}
+  		counter += 1;
   }
-
-  if(array[i] == ">"){
-
-    ++pointer;
-    
-	blocks.push({cmd : ">", mem : memory[pointer], pnt : pointer});
-
-  }else if(array[i] == "<"){
-
-    if(pointer > 0){
-
-     --pointer;
-     
-     	blocks.push({cmd : "<", mem : memory[pointer], pnt : pointer});
-
-    }else{
-
-     pointer = memory.length - 1;
-     
-     blocks.push({cmd : "<", mem : memory[pointer], pnt : pointer});
-
-    }
-
-  }else if(array[i] == "+"){
-
-      ++memory[pointer];
-      
-      	blocks.push({cmd : "+", mem : memory[pointer], pnt : pointer});
-
-  }else if(array[i] == "-"){
-
-      --memory[pointer];
-      
-      	blocks.push({cmd : "-", mem : memory[pointer], pnt : pointer});
-
-  }else if(array[i] == "."){
-
-    var cd = String.fromCharCode(memory[pointer]);
-    
-    result.push(cd);
-
-	blocks.push({cmd : ".", mem : memory[pointer], pnt : pointer, code : cd});
-  
-  }else if(array[i] == ","){
-
-    memory[pointer] = input.shift().charCodeAt(0);
-
-	blocks.push({cmd : ",", mem : memory[pointer], pnt : pointer});
-
-  }else if(array[i] == "["){
-
-    if(open.length){
-
-      if(open[open.length-1] != i){
-
-        open.push(i);
-
-      	blocks.push({cmd : "[", mem : memory[pointer], pnt : pointer});
-      }
-
-    }else{
-
-      open.push(i);
-
-    	blocks.push({cmd : "[", mem : memory[pointer], pnt : pointer});
-    }
-
-  }else if(array[i] == "]"){
-
-    if(open.length){
-
-      if(memory[pointer]){
-
-        i = open[open.length - 1];
-
-      	blocks.push({cmd : "]", mem : memory[pointer], pnt : pointer});
-      }else{
-
-        open.pop();
-
-      	blocks.push({cmd : "]", mem : memory[pointer], pnt : pointer});
-      }
-
-    }
-
-  }
-
-}
-
-return blocks;
-
+  //console.log(blocks);
+  return blocks;
 }
