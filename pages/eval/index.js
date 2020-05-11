a85f5c5c718ac0f9af629dae00d98eda634b1fcc
Miles MJ Jamon <31880601+pvzzombs@users.noreply.github.com>
2020-05-11 15:55:54 +0400

    Update index.js

--- pages/eval/index.js
+++ pages/eval/index.js
@@ -1,7 +1,14 @@
 function compute(string){
-  return Eval(string);
+  document.getElementById('history').innerText += "\n >>> " + string;
+  var ans = Eval(string);
+  document.getElementById('history').innerText += " = " + ans;
+  return ans;
 }
 
+function backspace(){
+  var temp = document.calculator.display.value;
+  document.calculator.display.value = temp.substr(0, temp.length -1);
+}
 $("document").ready(function(){
   function getParameterByName(name, url) {
     if (!url) url = window.location.href;
@@ -22,4 +29,19 @@ $("document").ready(function(){
   $("#app").show();
 
   $('.modal').modal();
+
+  var a = ["" , ""];
+  $("#display").on("input", function(){
+  	var b = m.getKey($("#display").val(), a);
+  	var tmp = $("#display").val().replace(/\n/,"");
+
+    try{
+        if(b[0] === "\n"){
+          var ot = compute(tmp);
+          $("#display").val(ot);
+        }
+    }catch(e){
+    	$("#display").val(e);
+    }
+  });
 });
